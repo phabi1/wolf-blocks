a5aec26d02e4123caeed9ddaf0893e15c366f3e2
Fabien Heilles <phabi1@hotmail.fr>
2025-12-04 08:08:31 +0100

    initialiaze project

---
+++ build/section/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '1674a6c4ddeca87c6907');
---
+++ build/section/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '01ffaea3ea7a7085a63a');
